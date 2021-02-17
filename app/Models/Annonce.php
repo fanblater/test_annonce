d67ee4cfbbe076e8939c1caa4f63bc70aa54cb98
fanblater <fanny.bellais@outlook.com>
2021-02-17 19:51:04 +0100

    update for sortable prix and surface

--- app/Models/Annonce.php
+++ app/Models/Annonce.php
@@ -6,16 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
+use Kyslik\ColumnSortable\Sortable;
 
 class Annonce extends Model
 {
     use HasFactory;
+    use Sortable;
 
+    public $timestamps = false;
 
     protected $table = "annonces";
 
     protected $cast = [
         'prix' => 'float'
+    ];
+
+    protected $sortable = [
+        'prix',
+        'surface'
     ];
 
     protected $fillable = [
